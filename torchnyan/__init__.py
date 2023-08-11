2fb97aba64e65982a1a92e63a572bc11add9de41
speedcell4 <speedcell4@gmail.com>
2023-08-11 19:05:23 +0900

    Feat: Add __init__.py

---
+++ torchnyan/__init__.py
@@ -0,0 +1,23 @@
+from torchnyan.assertion import assert_catted_sequence_close
+from torchnyan.assertion import assert_close
+from torchnyan.assertion import assert_grad_close
+from torchnyan.assertion import assert_packed_sequence_close
+from torchnyan.strategy import BATCH_SIZE
+from torchnyan.strategy import FEATURE_DIM
+from torchnyan.strategy import TINY_BATCH_SIZE
+from torchnyan.strategy import TINY_FEATURE_DIM
+from torchnyan.strategy import TINY_TOKEN_SIZE
+from torchnyan.strategy import TOKEN_SIZE
+from torchnyan.strategy import device
+from torchnyan.strategy import sizes
+
+__all__ = [
+    'assert_close', 'assert_grad_close',
+    'assert_catted_sequence_close',
+    'assert_packed_sequence_close',
+
+    'TINY_BATCH_SIZE', 'TINY_TOKEN_SIZE', 'TINY_FEATURE_DIM',
+    'BATCH_SIZE', 'TOKEN_SIZE', 'FEATURE_DIM',
+    'device',
+    'sizes',
+]
