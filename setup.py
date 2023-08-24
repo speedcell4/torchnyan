6893330647e3e1ba8e2d26c18f3b84f5674e3b7e
speedcell4 <speedcell4@gmail.com>
2023-08-24 19:04:16 +0900

    Chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
 name = 'torchnyan'
+
+root_dir = Path(__file__).parent.resolve()
+with (root_dir / 'requirements.txt').open(mode='r', encoding='utf-8') as fp:
+    install_requires = [install_require.strip() for install_require in fp]
 
 setup(
     name=name,
@@ -11,6 +17,6 @@ setup(
     license='MIT',
     author='speedcell4',
     author_email='speedcell4@gmail.com',
-    python_requires='>=3.9',
-    install_requires=[],
+    python_requires='>=3.8',
+    install_requires=install_requires,
 )
